ed576be61510c93f14818b294b16c19357171b16
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2025-03-14 19:13:22 +0100

    delete controller

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,35 +1,17 @@
 <?php
-    
-    use Core\Database;
-    
-    $config = require base_path('config.php');
-	
-	$db = new Database($config['database']);
-	
-	$heading = 'Note';
-    
-	$userId = 1;
-    
-    if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $note = $db->query('select * from notes where id = :id', ['id' => $_POST['id']])->findOrFail();
-        
-        authorize($note['user_id'] === $userId);
-        
-        $db->query('delete from notes where id = :id', ['id' => $_POST['id']]);
-        
-        header('Location: /notes');
-        
-        exit;
-    } else {
-        $note = $db->query(
-            "select * from notes where id = :id",
-            ['id' => $_GET['id']]
-        )->findOrFail();
-        
-        authorize($note['user_id'] === $userId);
-        
-        view('notes/show', [
-            'heading' => $heading,
-            'note' => $note
-        ]);   
-    }
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$heading = 'Note';
+
+$userId = 1;
+$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
+
+authorize($note['user_id'] === $userId);
+
+view('notes/show', [
+    'heading' => $heading, 'note' => $note
+]);   
--- routes.php
+++ routes.php
@@ -5,5 +5,6 @@ $router->get('/contact', 'controllers/contact.php');
 
 $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
+$router->delete('/note', 'controllers/notes/delete.php');
 $router->get('/note/create', 'controllers/notes/create.php');
 
