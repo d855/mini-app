5f769a5a0f47469597a9296a855e41eb584a95e4
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2025-03-27 19:21:11 +0100

    middleware

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 $router->get('/', 'controllers/index.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/delete.php');
 
@@ -13,7 +13,7 @@ $router->patch('/note', 'controllers/notes/update.php');
 $router->get('/note/create', 'controllers/notes/create.php');
 $router->post('/note', 'controllers/notes/store.php');
 
-$router->get('/register', 'controllers/auth/register/create.php');
+$router->get('/register', 'controllers/auth/register/create.php')->only('guest');
 $router->post('/register', 'controllers/auth/register/store.php');
 
 
